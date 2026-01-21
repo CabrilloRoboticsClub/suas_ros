32c8550f6111017c9ff4051ad0571405f1129440
StandardGitHubAcc <mide122006@gmail.com>
2026-01-20 20:16:24 -0800

    Use image subscriber node code from https://ibrahimmansur4.medium.com/integrating-opencv-with-ros2-a-comprehensive-guide-to-computer-vision-in-robotics-66b97fa2de92

--- src/suas_cv/setup.py
+++ src/suas_cv/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "suas_cv_imageSubscriber = suas_cv.test1:main"
         ],
     },
 )
--- src/suas_cv/suas_cv/test1.py
+++ src/suas_cv/suas_cv/test1.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python3
+#https://ibrahimmansur4.medium.com/integrating-opencv-with-ros2-a-comprehensive-guide-to-computer-vision-in-robotics-66b97fa2de92
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
 
-class MyNode(Node):
+class ImageSubscriber(Node):
     def __init__(self):
-        super().__init__("first_cv_node")
-        self.get_logger().info("Hello from first_cv_node")
+        super().__init__('image_subscriber')
+        self.subscription = self.create_subscription(
+            Image,
+            '/camera/image_raw',
+            self.listener_callback,
+            10)
+        self.br = CvBridge()
+    
+    def listener_callback(self, data):
+        self.get_logger().info('Receiving video frame')
+        # As pointed in comments below modify the following to use bgr encoding
+        # current_frame = self.br.imgmsg_to_cv2(data)
+        current_frame = self.br.imgmsg_to_cv2(data, desired_encoding='bgr8')
+        cv2.imshow("camera", current_frame)
+        cv2.waitKey(1)
 
 def main(args=None):
     rclpy.init(args=args)
-
-    node = MyNode()
-    rclpy.spin(node)
-
+    image_subscriber = ImageSubscriber()
+    rclpy.spin(image_subscriber)
+    image_subscriber.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
-
-
