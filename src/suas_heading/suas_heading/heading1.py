a849bcc9b2d6fbf2ebf8a77f410ebb1c1891924f
Michael Ide <mide122006@gmail.com>
2026-02-08 12:06:37 -0800

    Comments cleanup.

--- src/suas_heading/suas_heading/heading1.py
+++ src/suas_heading/suas_heading/heading1.py
@@ -31,20 +31,6 @@ class Heading(Node):
 
     def navsat_callback(self, msg):
 
-        #math.atan2(msg.latitude - self.lastPos["latitude"], msg.longitude - self.lastPos["longitude"])
-        #math.atan(math.sqrt(math.pow( msg.latitude - self.lastPos["latitude"],2) + math.pow(msg.longitude - self.lastPos["longitude"] ,2)))
-        # theta = math.atan2(msg.latitude - self.lastPos["latitude"], msg.longitude - self.lastPos["longitude"])
-
-        
-        # theta = math.degrees(theta)
-        # #tmp = (tmp + 360) % 360
-
-        # beta = (90 - theta) % 360
-        # #if beta < 0:
-        # #    beta += 360
-
-        # self.lastHeading = beta
-
         if self.lastPos["latitude"] != msg.latitude or self.lastPos["longitude"] != msg.longitude:
             
             #https://forum.arduino.cc/t/get-the-direction-using-gps-without-compass/502005/3
@@ -55,7 +41,6 @@ class Heading(Node):
 
         
             theta = math.degrees(theta)
-            #tmp = (tmp + 360) % 360
 
             beta = (90 - theta) % 360
             #if beta < 0:
@@ -63,38 +48,16 @@ class Heading(Node):
             
             self.lastHeading = beta
 
-
-            #logging.info(f'tan {math.degrees(math.atan(math.sqrt(math.pow( msg.latitude - self.lastPos["latitude"],2) + math.pow(msg.longitude - self.lastPos["longitude"] ,2))))}')
-            
-            # lat1 = self.lastPos["latitude"]
-            # lat2 = msg.latitude
-            # long1 = self.lastPos["longitude"]
-            # long2 = msg.longitude
-
-            # y = math.sin(long2 - long1) * math.cos(lat2)
-            # x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(long2 - long1)
-            # bearing = math.atan2(y, x)
-            # logging.info(f"bearing: {math.degrees(bearing)}")
-
             # bearing is from [-180, 180)
             # theta is from [0, 360)
             # moving red arrow in gazebo to the left is towards true north (~360/0 segrees off of true north)
             # moving red arrow in gazebo to the right is towards true south (~180/-180 degrees off of true north)
             # moving green arrow in gazebo away is towards west
             # moving green arrow in gazebo closer is towards east
-            # seems like beta and bearing are counting in opposite directions
-            #   when beta is 359.99974057483695, bearing is 0.00017966901917246036
-            #   when beta is 89.99987641761386, bearing is -90.0003406425087
-            #   when beta is 179.99974056364942, bearing is -179.99982033705527
-            #   when beta is 269.99985104334064, bearing is 90.00075672182112
-
-            #   when beta is 219.9971989426276, bearing is 149.8402788532371
-            #   when beta is 26.559046348171755, bearing is -19.095648566865947
-
-            # confirmed that when taking a measurement and moving in the same direction without changing angle, 
-            #   the measurement by lastHeading and bearing will be the same as the previous measurement
-            # not sure if it would be more useful to have angle from north in range [-180, 180) (bearing) or [0, 360) (beta)
-            # "bearing" DOES NOT WORK PROPERLY BUT BETA DOES. DO NOT USE BEARING
+        
+            # beta is in range [0, 360) degrees
+            # "bearing" DOES NOT WORK PROPERLY BUT BETA DOES. DO NOT USE BEARING.
+            # with beta (heading), west is 270 degrees, east is 90 degrees, south is 180 degrees, north is 0 degrees
 
             self.lastPos["latitude"] = msg.latitude
             self.lastPos["longitude"] = msg.longitude
